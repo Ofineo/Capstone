f373af493289d958010f6c031e129f77a31a0145
Jordi <ofineo@gmail.com>
2020-03-27 10:07:28 +0000

    added new token to test_capstone.py

--- test_Capstone.py
+++ test_Capstone.py
@@ -6,6 +6,7 @@ from app import create_app
 from models import setup_db, Actor, Movie
 import datetime
 
+TOKEN_PRODUCER = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUzMDAyMjUsImV4cCI6MTU4NTM4NjYyNSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.WSJMNDMlOy7bkcJMePO1RKKSHpfcxdbcC_GCOepLKYgBRRfuuDUt76555m1ZD2V0FYlcicpquA0uqqvOXyt-4aepqbUvauI02zt-7aALae8cbYlqvulcSrPTzfON_wJu3YSdLjYtHbvoLg7zL-umTTI4Fozb63UPbQGb_rQD9IwU_ud8s7wb1mCSqpZzczNbI7FrYp2F-I5T1gEY3zvAOtygasC5d-jQ8t3mYRq4GJcROQ83bolkrI0fX4ynIxqow2vh89zBb6E8KZt1vly0IuocViK_4XRGr5fhLc4vgusjhdua-R4Pr6zjP4sTX4Mq-b16JfKGFvoUNh9DwpUOtw'
 
 class CapstoneTestCase(unittest.TestCase):
 
@@ -48,7 +49,9 @@ class CapstoneTestCase(unittest.TestCase):
         pass
 
     def test_get_all_actors(self):
-        res = self.client().get('/actors', headers={'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUyMTE3ODksImV4cCI6MTU4NTI5ODE4OSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.s2ZdhS_O7cTCaD92vHdz_nnCI02VG4w4lWy_C5IOi7Ubx5O4HLEJUFKlqSXLOfAGnZrk1bwvhtC2uFcMT6dG8PFvS4zaWeMKVMQM0mFKKnAoEX_y9vXXyBAtj3tl7NqUqyYgK-Mtv-7-vaNnEC6wFNEzchkpAHU6M0KZ__RGcuuAWY9cSBClFiZmcc4CmHeO9GJJCjxau-gzPa9nEG7bvUkEFUGk4evu-ELPG4a9Nmna7JMVQ3eSry3SZnm0Z_uyzV7nUtHcWoD0Bc-xyuF5mt8NFm37PaHyaDLaypiwJ_HfoaWSNCucwuZfXTtZ76w_TvBToqOj9qo40zfOtOeYeQ'})
+        res = self.client().get('/actors', headers={
+            'Authorization': TOKEN_PRODUCER}
+        )
         data = res.get_json()
 
         selection = Actor.query.all()
@@ -60,7 +63,7 @@ class CapstoneTestCase(unittest.TestCase):
         table_length = Actor.query.all()
 
         res = self.client().post('/actors/add', headers={
-            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUyMTE3ODksImV4cCI6MTU4NTI5ODE4OSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.s2ZdhS_O7cTCaD92vHdz_nnCI02VG4w4lWy_C5IOi7Ubx5O4HLEJUFKlqSXLOfAGnZrk1bwvhtC2uFcMT6dG8PFvS4zaWeMKVMQM0mFKKnAoEX_y9vXXyBAtj3tl7NqUqyYgK-Mtv-7-vaNnEC6wFNEzchkpAHU6M0KZ__RGcuuAWY9cSBClFiZmcc4CmHeO9GJJCjxau-gzPa9nEG7bvUkEFUGk4evu-ELPG4a9Nmna7JMVQ3eSry3SZnm0Z_uyzV7nUtHcWoD0Bc-xyuF5mt8NFm37PaHyaDLaypiwJ_HfoaWSNCucwuZfXTtZ76w_TvBToqOj9qo40zfOtOeYeQ'},
+            'Authorization': TOKEN_PRODUCER},
             json=self.new_actor
         )
 
@@ -79,8 +82,11 @@ class CapstoneTestCase(unittest.TestCase):
         actor.insert()
 
         res = self.client().patch('/actors/222', headers={
-            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUyMTE3ODksImV4cCI6MTU4NTI5ODE4OSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.s2ZdhS_O7cTCaD92vHdz_nnCI02VG4w4lWy_C5IOi7Ubx5O4HLEJUFKlqSXLOfAGnZrk1bwvhtC2uFcMT6dG8PFvS4zaWeMKVMQM0mFKKnAoEX_y9vXXyBAtj3tl7NqUqyYgK-Mtv-7-vaNnEC6wFNEzchkpAHU6M0KZ__RGcuuAWY9cSBClFiZmcc4CmHeO9GJJCjxau-gzPa9nEG7bvUkEFUGk4evu-ELPG4a9Nmna7JMVQ3eSry3SZnm0Z_uyzV7nUtHcWoD0Bc-xyuF5mt8NFm37PaHyaDLaypiwJ_HfoaWSNCucwuZfXTtZ76w_TvBToqOj9qo40zfOtOeYeQ'},
-            json={"name": "Otilio", "age": "54"}
+            'Authorization': TOKEN_PRODUCER},
+            json={
+                "name": "Otilio", 
+                "age": "54"
+            }
         )
         data = res.get_json()
 
@@ -101,7 +107,7 @@ class CapstoneTestCase(unittest.TestCase):
         actor.insert()
 
         res = self.client().delete('/actors/222', headers={
-            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUyMTE3ODksImV4cCI6MTU4NTI5ODE4OSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.s2ZdhS_O7cTCaD92vHdz_nnCI02VG4w4lWy_C5IOi7Ubx5O4HLEJUFKlqSXLOfAGnZrk1bwvhtC2uFcMT6dG8PFvS4zaWeMKVMQM0mFKKnAoEX_y9vXXyBAtj3tl7NqUqyYgK-Mtv-7-vaNnEC6wFNEzchkpAHU6M0KZ__RGcuuAWY9cSBClFiZmcc4CmHeO9GJJCjxau-gzPa9nEG7bvUkEFUGk4evu-ELPG4a9Nmna7JMVQ3eSry3SZnm0Z_uyzV7nUtHcWoD0Bc-xyuF5mt8NFm37PaHyaDLaypiwJ_HfoaWSNCucwuZfXTtZ76w_TvBToqOj9qo40zfOtOeYeQ'},
+            'Authorization': TOKEN_PRODUCER},
         )
 
         data = res.get_json()
@@ -111,7 +117,9 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(selection, None)
 
     def test_get_all_movies(self):
-        res = self.client().get('/movies', headers={'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUyMTE3ODksImV4cCI6MTU4NTI5ODE4OSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.s2ZdhS_O7cTCaD92vHdz_nnCI02VG4w4lWy_C5IOi7Ubx5O4HLEJUFKlqSXLOfAGnZrk1bwvhtC2uFcMT6dG8PFvS4zaWeMKVMQM0mFKKnAoEX_y9vXXyBAtj3tl7NqUqyYgK-Mtv-7-vaNnEC6wFNEzchkpAHU6M0KZ__RGcuuAWY9cSBClFiZmcc4CmHeO9GJJCjxau-gzPa9nEG7bvUkEFUGk4evu-ELPG4a9Nmna7JMVQ3eSry3SZnm0Z_uyzV7nUtHcWoD0Bc-xyuF5mt8NFm37PaHyaDLaypiwJ_HfoaWSNCucwuZfXTtZ76w_TvBToqOj9qo40zfOtOeYeQ'})
+        res = self.client().get('/movies', headers={
+            'Authorization': TOKEN_PRODUCER}
+            )
         data = res.get_json()
 
         selection = Movie.query.all()
@@ -125,7 +133,7 @@ class CapstoneTestCase(unittest.TestCase):
         table_length = Movie.query.all()
 
         res = self.client().post('/movies/add', headers={
-            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUyMTE3ODksImV4cCI6MTU4NTI5ODE4OSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.s2ZdhS_O7cTCaD92vHdz_nnCI02VG4w4lWy_C5IOi7Ubx5O4HLEJUFKlqSXLOfAGnZrk1bwvhtC2uFcMT6dG8PFvS4zaWeMKVMQM0mFKKnAoEX_y9vXXyBAtj3tl7NqUqyYgK-Mtv-7-vaNnEC6wFNEzchkpAHU6M0KZ__RGcuuAWY9cSBClFiZmcc4CmHeO9GJJCjxau-gzPa9nEG7bvUkEFUGk4evu-ELPG4a9Nmna7JMVQ3eSry3SZnm0Z_uyzV7nUtHcWoD0Bc-xyuF5mt8NFm37PaHyaDLaypiwJ_HfoaWSNCucwuZfXTtZ76w_TvBToqOj9qo40zfOtOeYeQ'},
+            'Authorization': TOKEN_PRODUCER},
             json=self.new_movie
         )
 
@@ -144,8 +152,11 @@ class CapstoneTestCase(unittest.TestCase):
         movie.insert()
 
         res = self.client().patch('/movies/222', headers={
-            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUyMTE3ODksImV4cCI6MTU4NTI5ODE4OSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.s2ZdhS_O7cTCaD92vHdz_nnCI02VG4w4lWy_C5IOi7Ubx5O4HLEJUFKlqSXLOfAGnZrk1bwvhtC2uFcMT6dG8PFvS4zaWeMKVMQM0mFKKnAoEX_y9vXXyBAtj3tl7NqUqyYgK-Mtv-7-vaNnEC6wFNEzchkpAHU6M0KZ__RGcuuAWY9cSBClFiZmcc4CmHeO9GJJCjxau-gzPa9nEG7bvUkEFUGk4evu-ELPG4a9Nmna7JMVQ3eSry3SZnm0Z_uyzV7nUtHcWoD0Bc-xyuF5mt8NFm37PaHyaDLaypiwJ_HfoaWSNCucwuZfXTtZ76w_TvBToqOj9qo40zfOtOeYeQ'},
-            json={"title": "avengers vs godzilla", "releaseDate": "2021-03-25 11:55:11.271041"}
+            'Authorization': TOKEN_PRODUCER},
+            json={
+                "title": "avengers vs godzilla", 
+                "releaseDate": "2021-03-25 11:55:11.271041"
+            }
         )
 
         data = res.get_json()
@@ -166,7 +177,7 @@ class CapstoneTestCase(unittest.TestCase):
         movie.insert()
 
         res = self.client().delete('/movies/222', headers={
-            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9USXpNakl6UkVZMFJEQXdNRGN3UTBFNVF6TTBNekE0TURNMVF6bERRVGRFUVRNeE1VUXpNdyJ9.eyJpc3MiOiJodHRwczovL29maW5lby5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU3YzY4Y2ExMWRkNDkwYzZiM2VlNTM0IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODUyMTE3ODksImV4cCI6MTU4NTI5ODE4OSwiYXpwIjoiMzVNNTJYbEgxM1R6NDl6T3gzQXBkUTdrNmY3Y1Fnb2wiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvcnMiLCJkZWxldGU6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.s2ZdhS_O7cTCaD92vHdz_nnCI02VG4w4lWy_C5IOi7Ubx5O4HLEJUFKlqSXLOfAGnZrk1bwvhtC2uFcMT6dG8PFvS4zaWeMKVMQM0mFKKnAoEX_y9vXXyBAtj3tl7NqUqyYgK-Mtv-7-vaNnEC6wFNEzchkpAHU6M0KZ__RGcuuAWY9cSBClFiZmcc4CmHeO9GJJCjxau-gzPa9nEG7bvUkEFUGk4evu-ELPG4a9Nmna7JMVQ3eSry3SZnm0Z_uyzV7nUtHcWoD0Bc-xyuF5mt8NFm37PaHyaDLaypiwJ_HfoaWSNCucwuZfXTtZ76w_TvBToqOj9qo40zfOtOeYeQ'},
+            'Authorization': TOKEN_PRODUCER},
         )
 
         data = res.get_json()
